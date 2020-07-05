852e0061f61a319758fc69b9165eeb36ea1c6728
Shawn Benny <sbenn9210@gmail.com>
2020-07-05 10:34:22 -0500

    fizzbuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -13,17 +13,50 @@
 //   buzz
 
 function fizzBuzz(n) {
-  for(var i = 1; i <= n; i++) {
-    if(i % 15 == 0) {
-      console.log('fizzbuzz')
-    } else if(i % 3 == 0) {
-      console.log('fizz')
-    }else if(i % 5 == 0) {
-      console.log('buzz')
-    }else {
-      console.log(i)
+  for (var i = 1; i <= n; i++) {
+    if (i % 15 == 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 == 0) {
+      console.log("fizz");
+    } else if (i % 5 == 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
     }
   }
 }
-fizzBuzz(15)
+fizzBuzz(15);
 module.exports = fizzBuzz;
+
+// Write a function "fizzbuzz" that returns the fizzbuzz string for an input number.
+// A fizzbuzz string is defined as the following:
+// For every number from 1 to the input number
+// - if that number is not a multiple of 3 or 5, add a period "." to the fizzbuzz string
+// - for every number that is a multiple of 3 (but not 5), add "fizz" to the fizzbuzz string
+// - for every number that is a multiple of 5 (but not 3), add "buzz" to the fizzbuzz string
+// - for every number that is a multiple of 3 and 5, add "fizzbuzz" to the fizzbuzz string
+//
+// Examples:
+// fizzbuzz(3) --> '..fizz'
+// fizzbuzz(15) --> '..fizz.buzzfizz..fizzbuzz.fizz..fizzbuzz'
+
+function fizzbuzz(num) {
+  var arr = [];
+  for (var i = 1; i <= num; i++) {
+    if (i % 3 != 0 && i % 5 != 0) {
+      arr.push(".");
+    }
+    if (i % 3 == 0 && i % 5 != 0) {
+      arr.push("fizz");
+    }
+    if (i % 3 != 0 && i % 5 == 0) {
+      arr.push("buzz");
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+      arr.push("fizzbuzz");
+    }
+  }
+  console.log(arr.join(""));
+}
+
+fizzbuzz(15);
